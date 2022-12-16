ac045a7a41cf32c3c8b374548bcf613bbaa133b6
勞友刑✨ <sjmjgd1165@gmail.com>
2022-12-16 11:48:46 +0800

    Update CustomComponent.ts

--- Work/Zilu/FoodTrain/CustomComponent.ts
+++ Work/Zilu/FoodTrain/CustomComponent.ts
@@ -13,14 +13,14 @@ export class PnsHelpFaqComponent implements OnInit, OnDestroy {
   component$: Observable<E2HelpFaqCMSComponent> = this.cmsService
     .getContentSlot('FoodAngelFaq')
     .pipe(
-      map(p => p.components),
+      map(p => p.components), // map 將一個訂閱可以得到的資料轉換成另外一筆資料
       map(components =>
         components.filter(item => item.typeCode === 'E2HelpFaqComponent')
       ),
-      switchMap(components =>
+      switchMap(components => // switchMap 收到 observable 資料時，轉換成另外一個 observable
         this.componentData.data$.pipe(
-          take(1),
-          tap(data => {
+          take(1), // 取开头n个值
+          tap(data => { // tap 什麼都不影響可以在這做某些動作
             if (data && components.length > 0) {
               this.isActive$.next(data.uid === components[0].uid);
             }
@@ -29,7 +29,7 @@ export class PnsHelpFaqComponent implements OnInit, OnDestroy {
       )
     );
 
-  isActive$ = new BehaviorSubject(false);
+  isActive$ = new BehaviorSubject(false); // 可以有初始值，一註冊馬上拿到值 https://blog.kevinyang.net/2017/02/26/rxjs-context-demo-3/
 
   constructor(
     private cmsService: CmsService,
