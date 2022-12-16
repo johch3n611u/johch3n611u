4a4989abcb0aa0f11565292efabb9a417f397ddb
勞友刑✨ <sjmjgd1165@gmail.com>
2022-12-16 11:18:32 +0800

    Update Startacus.module.ts

--- Work/Zilu/FoodTrain/Startacus.module.ts
+++ Work/Zilu/FoodTrain/Startacus.module.ts
@@ -1,19 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ConfigModule } from '@spartacus/core';
-import { PnsHelpFaqComponent } from './pns-help-faq.component';
+import { PnsFaqComponent } from './pns-faq.component';
 
 @NgModule({
-  declarations: [PnsHelpFaqComponent],
+  declarations: [PnsFaqComponent],
   imports: [
     CommonModule, // @angular/common 基礎的 Angular 指令和管道， 例如 NgIf、NgForOf 由 Angular Core 提供
     ConfigModule.withConfig({ // @Spartacus/core 模組
       cmsComponents: { // 較複雜的 cmsComponent 要配合 ts 檔案內的語法去置換 Component 可參考 https://www.youtube.com/watch?v=3xhnYUK9-sc&ab_channel=Divante
         E2HelpFaqComponent: { // 要被取代的元件的 impex 資料內的typeCode
-          component: PnsHelpFaqComponent, // 客製化的元件
+          component: PnsFaqComponent, // 客製化的元件
         },
       },
     }),
   ],
 })
-export class PnsHelpFaqModule {}
+export class PnsFaqModule { }
