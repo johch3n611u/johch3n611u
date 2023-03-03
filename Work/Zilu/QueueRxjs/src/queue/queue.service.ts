f815bca0d09db78081af2ed96b738628471a4890
johch3n611u <sjmjgd1165@gmail.com>
2023-03-03 16:43:43 +0800

    補充註解

--- Work/Zilu/QueueRxjs/src/queue/queue.service.ts
+++ Work/Zilu/QueueRxjs/src/queue/queue.service.ts
@@ -8,8 +8,11 @@ import { Observable, from } from 'rxjs';
 })
 export class QueueService {
 
+  // 緩衝不立即發出的資料
   private pending$ = new BehaviorSubject<any>([]);
+  // 等待發出的資料
   public outgoing$ = new BehaviorSubject<any>([]);
+  // 當符合條件時發出資料
   private isReadyToEmit$ = new Subject<boolean>();
   private ngUnsubscribe = new Subject();
 
@@ -18,14 +21,18 @@ export class QueueService {
 
   constructor() {
     this.getPendingEvent().pipe(
+      // 發出符合給定條件的值
       filter(items => items && items.length > 0),
+      // 根據一個選擇器函數，捨棄掉在兩次輸出之間小於指定時間的發出值
       debounce(items =>
         timer(
           items.length >= Number(this.BUFFER_SIZE) ? 0 : (Number(this.DEBOUNCE_TIME))
         ),
       ),
+      // 發出值，直到提供的 observable 發出值，它便完成
       takeUntil(this.ngUnsubscribe)
     ).subscribe(pendingItems => {
+      // 分類：超過 BUFFER_SIZE 筆數則等待下一次發送
       const outgoingList = pendingItems.slice(0, Number(this.BUFFER_SIZE)) || [];
       const restOfPendingList = pendingItems.slice(outgoingList.length, pendingItems.length) || [];
       if (outgoingList.length > 0) {
@@ -33,7 +40,7 @@ export class QueueService {
         this.isReadyToEmit$.next(true);
       }
       this.pending$.next(restOfPendingList);
-    })
+    });
 
     this.isReadyToEmit$.pipe(
       withLatestFrom(this.getOutgoingEvent()),
@@ -45,10 +52,11 @@ export class QueueService {
       res => {
         this.clearOutgoingEvents();
       }
-    )
+    );
   }
 
   ngOnDestroy() {
+    console.log('ngOnDestroy');
     this.ngUnsubscribe.complete();
   }
 
